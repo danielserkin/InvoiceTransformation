84e2815e381a314221a6b9799fb94d6f41eb5add
daniel.serkin@fravega.com.ar <Daniel.Serkin@fravega.com.ar>
2024-11-15 13:05:46 -0300

    Add project files.

---
+++ Controllers/TransformController.cs
@@ -0,0 +1,51 @@
+﻿using InvoiceTransformationAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceTransformationAPI.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransformController : ControllerBase
+    {
+        private readonly ITransformService _transformService;
+
+        public TransformController(ITransformService transformService)
+        {
+            _transformService = transformService;
+        }
+
+        [HttpPost("endpoint-name")]
+        public IActionResult TransformXmlToJson([FromBody] TransformRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Xml))
+            {
+                return BadRequest(new
+                {
+                    status = "400",
+                    title = "Bad Request",
+                    detail = "The 'xml' field is required and cannot be empty."
+                });
+            }
+
+            try
+            {
+                var jsonResponse = _transformService.TransformBase64XmlToJson(request.Xml);
+                return Ok(jsonResponse);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new
+                {
+                    status = "500",
+                    title = "Internal Server Error",
+                    detail = ex.Message
+                });
+            }
+        }
+    }
+
+    public class TransformRequest
+    {
+        public string Xml { get; set; }
+    }
+}
